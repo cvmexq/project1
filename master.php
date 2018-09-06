576d9968ebdccb26360c71fe7d01d45a3f0fe223
StaffSistemas <43004710+StaffSistemas@users.noreply.github.com>
2018-09-06 09:08:09 -0500

    Add files via upload
    perdón por la tardanza u.u,

---
+++ master.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<p>Hola a todos como estan?</p>
+		<p>Ya esta el cruce con cuatro facturas pendientes por ver</p>
+	</head>
+	<body>
+
+	</body>
+</html>

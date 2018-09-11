2d3bcc9ae68eb992a13c3c549f45c5601f140963
Martín Sánchez Ruiz <43004931+Developer2MexQ@users.noreply.github.com>
2018-09-11 09:18:55 -0500

    AprendiendoAjax
    Aprendi ajax :v

---
+++ AjaxMartin/Ajax1.js
@@ -0,0 +1,19 @@
+console.log('activo');
+document.querySelector('#boton').addEventListener('click',traerDatos);
+
+function traerDatos()
+{
+ //console.log('Hola buena noshe')
+
+ const xhttp = new XMLHttpRequest();
+ xhttp.open('GET','archivo.txt', true);
+ xhttp.send();
+ xhttp.onreadystatechange = function()
+ {
+   if(this.readyState == 4 && this.status == 200)
+   {
+     //console.log(this.responseText);
+     document.querySelector('#respuesta').innerHTML = this.responseText;
+   }
+ }
+}
